b237dfbbf78f43b30298ab09c7a5570b12dbef88
mrSom3a <mrsom3a8@gmail.com>
2024-07-22 17:16:02 +0400

    making menu for mobile screen

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const navbar = document.getElementsByTagName("nav");
+const navToggle = document.getElementById('navToggle');
+const menuContainer = document.getElementById('containerMenu')
+const closeToggle = document.getElementById('closeToggle');
 
+
+closeToggle.onclick= ()=>{
+menuContainer.classList.remove("active")
+}
+
+navToggle.onclick =()=>{
+    menuContainer.classList.add("active")
+}
