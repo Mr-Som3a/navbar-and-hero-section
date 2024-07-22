3a461a02c257cd0551f0c46c54684a3edb6af28c
mrSom3a <mrsom3a8@gmail.com>
2024-07-22 21:48:48 +0400

    finish of mobile screen design

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const navToggle = document.getElementById('navToggle');
 const menuContainer = document.getElementById('containerMenu')
 const closeToggle = document.getElementById('closeToggle');
 
+const featureDetails = document.getElementById('feature')
+const companyDetails = document.getElementById('company')
+
+const details = document.getElementById('details')
+const f_icon = document.getElementById('f-icon')
+const c_icon = document.getElementById('c-icon')
+
+
 
 closeToggle.onclick= ()=>{
 menuContainer.classList.remove("active")
@@ -10,3 +18,32 @@ menuContainer.classList.remove("active")
 navToggle.onclick =()=>{
     menuContainer.classList.add("active")
 }
+
+featureDetails.onclick =(e)=>{
+    
+    const det = e.target.children[1]
+    if(det.className.includes('active')){
+        det.classList.remove('active')
+        f_icon.classList.remove('up')
+        f_icon.classList.add('down')
+
+    }else{
+        det.classList.add('active')
+        f_icon.classList.remove('down')
+        f_icon.classList.add('up')
+
+    }
+}
+companyDetails.onclick =(e)=>{
+    const det = e.target.children[1]
+    if(det.className.includes('active')){
+        det.classList.remove('active')
+        c_icon.classList.remove('up')
+        c_icon.classList.add('down')
+    }else{
+        det.classList.add('active')
+        c_icon.classList.remove('down')
+        c_icon.classList.add('up')
+    }
+}
+
