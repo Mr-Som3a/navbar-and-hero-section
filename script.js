03b5e3daa06494a88d6aad6d268deb4f9c87bd65
mrSom3a <mrsom3a8@gmail.com>
2024-07-25 15:02:53 +0400

    edit 01

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const closeToggle = document.getElementById('closeToggle');
 
 const featureDetails = document.getElementById('feature')
 const companyDetails = document.getElementById('company')
+const mFeatureDetails = document.getElementById('mFeature')
+const mCompanyDetails = document.getElementById('mCompany')
 
 const f_icon = document.getElementById('f-icon')
 const c_icon = document.getElementById('c-icon')
 
 
-
+console.log(featureDetails)
 closeToggle.onclick= ()=>{
 menuContainer.classList.remove("active")
 }
@@ -19,7 +21,6 @@ navToggle.onclick =()=>{
 }
 
 featureDetails.onclick =(e)=>{
-    console.log('init')
     const det = e.target.children[1]
     if(det.className.includes('active')){
         det.classList.remove('active')
@@ -34,6 +35,30 @@ featureDetails.onclick =(e)=>{
     }
 }
 companyDetails.onclick =(e)=>{
+    const det = e.target.children[1]
+    if(det.className.includes('active')){
+        det.classList.remove('active')
+        c_icon.classList.remove('up')
+        c_icon.classList.add('down')
+    }else{
+        det.classList.add('active')
+        c_icon.classList.remove('down')
+        c_icon.classList.add('up')
+    }
+}
+mFeatureDetails.onclick =(e)=>{
+    const det = e.target.children[1]
+    if(det.className.includes('active')){
+        det.classList.remove('active')
+        c_icon.classList.remove('up')
+        c_icon.classList.add('down')
+    }else{
+        det.classList.add('active')
+        c_icon.classList.remove('down')
+        c_icon.classList.add('up')
+    }
+}
+mCompanyDetails.onclick =(e)=>{
     const det = e.target.children[1]
     if(det.className.includes('active')){
         det.classList.remove('active')
